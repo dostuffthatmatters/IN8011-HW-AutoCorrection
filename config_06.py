7e63ffab045948e4353971f6ab9b499af95b370b
Moritz Makowski <makowskimoritz@gmail.com>
2019-12-16 00:40:52 +0100

    Added config 6

---
+++ config_06.py
@@ -0,0 +1,26 @@
+
+# Inside here you put all the files that have been provided for
+# this homework -> The ones they didn't have to write by themselves,
+# e.g. the testing "main"-file
+GIVEN_DIRECTORY = "HW06/given"
+
+# Inside here you put each submittees folder that you've downloaded
+# from moodle -> In here are all the "..._assignsubmission_file_"-folders
+SUBMISSION_DIRECTORY = "HW06/submission"
+
+
+
+# The file that we provided them with
+GIVEN_FILES = ["swap.h", "main_swap_correction.c"]
+
+# The files that are required to be in the submitted zip-file
+SUBMISSION_FILES = ["swap.c"]
+
+# These are alle the files that should be compiled and linked together
+# May not be equal to SUBMISSION_FILES + HOMEWORK_FILES
+FILES_TO_COMPILE = ["main_swap_correction.c", "swap.c"]
+
+
+
+# Where should the resulting markdown file be put
+PROTOCOL_LOCATION = "testing_protocol_hw06.md"
